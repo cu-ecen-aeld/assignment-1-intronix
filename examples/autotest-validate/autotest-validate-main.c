0203dadf09e75e689b7c694c5e4edbc22be3ba09
intronix <krishna.slanger@gmail.com>
2024-09-09 14:55:17 +0200

    First Assignment

--- examples/autotest-validate/autotest-validate-main.c
+++ examples/autotest-validate/autotest-validate-main.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 #include "autotest-validate.h"
 
-const char * git_username = "akshita-bhasin";
+const char * git_username = "intronix";
 
 int main(int argc, char**argv)
 {
+
+    printf("Username: %s\n", my_username());
+
     printf("this_function_returns_true returned %s\n",
                 this_function_returns_true() ? "true" : "false");
     printf("this_function_returns_false returned %s\n",
--- student-test/assignment1/Test_validate_username.c
+++ student-test/assignment1/Test_validate_username.c
@@ -14,9 +14,16 @@
 */
 void test_validate_my_username()
 {
-    /**
-     * TODO: Replace the line below with your code here as described above to verify your /conf/username.txt 
-     * config file and my_username() functions are setup properly
-     */
-    TEST_ASSERT_TRUE_MESSAGE(false,"AESD students, please fix me!");
+    const char *hardcoded_username = my_username();
+    
+    char *conf_file_username = malloc_username_from_conf_file();
+    
+    if (conf_file_username == NULL) {
+        TEST_FAIL_MESSAGE("Failed to read username from configuration file.");
+        return;
+    }
+    
+    TEST_ASSERT_EQUAL_STRING_MESSAGE(hardcoded_username, conf_file_username,"The usernames do not match.");
+    
+    free(conf_file_username);
 }
